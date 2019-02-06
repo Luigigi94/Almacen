0a061a235a104d59b133476019270950c6ad0675
LorenaVilchis <LorenaVilchis@users.noreply.github.com>
2019-02-06 14:48:35 -0600

    Add files via upload

--- almacen.sql
+++ almacen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-02-2019 a las 11:46:44
+-- Tiempo de generación: 06-02-2019 a las 19:50:52
 -- Versión del servidor: 10.1.30-MariaDB
--- Versión de PHP: 7.2.2
+-- Versión de PHP: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,62 +29,78 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `almacen` (
-  `id` int(11) NOT NULL,
-  `articulo_id` int(11) DEFAULT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `fecha_alta` date DEFAULT NULL,
-  `users_id` int(11) DEFAULT NULL,
-  `fecha_update` date DEFAULT NULL
+  `id_almacen` int(11) NOT NULL,
+  `id_articulo` int(11) DEFAULT NULL,
+  `f_alta` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `articulos`
+-- Estructura de tabla para la tabla `articulo`
 --
 
-CREATE TABLE `articulos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `codigo` varchar(100) DEFAULT NULL,
-  `precio` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cafeteria`
---
-
-CREATE TABLE `cafeteria` (
-  `id` int(11) NOT NULL,
-  `articulo_id` int(11) DEFAULT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `users_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
+CREATE TABLE `articulo` (
+  `id_articulo` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT NULL,
-  `is_admin` int(11) DEFAULT NULL
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_cattipo` int(11) DEFAULT NULL,
+  `codigo` int(11) DEFAULT NULL,
+  `precio_unico` float DEFAULT NULL,
+  `precio_mayoreo` float DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `users`
+-- Estructura de tabla para la tabla `categoria`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `nombre`, `is_admin`) VALUES
-(1, 'mutsotool@gmail.com', '1', 'Administrador', 1),
-(2, 'asd@gm.com', 'asd', 'Gerente', 0);
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_tipo`
+--
+
+CREATE TABLE `categoria_tipo` (
+  `id_cattipo` int(11) NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sucursal`
+--
+
+CREATE TABLE `sucursal` (
+  `id_sucursal` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `lugar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `id_puesto` int(11) DEFAULT NULL,
+  `contraseña` varchar(100) DEFAULT NULL,
+  `id_sucursal` int(11) DEFAULT NULL,
+  `contraseña2` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tablas volcadas
@@ -94,35 +110,77 @@ INSERT INTO `users` (`id`, `email`, `password`, `nombre`, `is_admin`) VALUES
 -- Indices de la tabla `almacen`
 --
 ALTER TABLE `almacen`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_almacen`);
 
 --
--- Indices de la tabla `articulos`
+-- Indices de la tabla `articulo`
 --
-ALTER TABLE `articulos`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`id_articulo`);
 
 --
--- Indices de la tabla `cafeteria`
+-- Indices de la tabla `categoria`
 --
-ALTER TABLE `cafeteria`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
 
 --
--- Indices de la tabla `users`
+-- Indices de la tabla `categoria_tipo`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `categoria_tipo`
+  ADD PRIMARY KEY (`id_cattipo`);
+
+--
+-- Indices de la tabla `sucursal`
+--
+ALTER TABLE `sucursal`
+  ADD PRIMARY KEY (`id_sucursal`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT de la tabla `almacen`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `almacen`
+  MODIFY `id_almacen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `id_articulo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_tipo`
+--
+ALTER TABLE `categoria_tipo`
+  MODIFY `id_cattipo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sucursal`
+--
+ALTER TABLE `sucursal`
+  MODIFY `id_sucursal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
